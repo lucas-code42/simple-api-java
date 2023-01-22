3eaa2908aec4170bed76da14155dabb11e3edb1e
lucas-code42 <lucasdearaujo.brandao@gmail.com>
2023-01-22 18:49:52 -0300

    initial commit

---
+++ src/main/java/api/prefeituras/PrefeiturasApplication.java
@@ -0,0 +1,13 @@
+package api.prefeituras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrefeiturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrefeiturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/prefeituras/PrefeiturasApplicationTests.java
@@ -0,0 +1,13 @@
+package api.prefeituras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrefeiturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
